319f441c9a3738ebea50d64a63176521b8aa0a69
Hiroshi Nagata <hirosn@live.com>
2014-10-13 15:38:30 -0700

    Add notes on changing default behavior.

--- ie8/script.js
+++ ie8/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
   var showNodes;
   var getTarget;
-  var showId;
 
   // Timestamp
   $('#timestamp').text(document.lastModified);
@@ -33,54 +32,64 @@ $(document).ready(function() {
   $('#page').append('<p>' + navigator.userAgent + '</p');
 
   // Event listeners and handlers
-  (function() {
+  getTarget = function(event) {
+    var target;
+    event = event || window.event;
+
+    if (event) {
+      target = event.target || event.srcElement;
+    }
+
+    return target;
+  };
+
+  listener($('#click1')).jQueryEvent(
+    'click', '#click1');
+
+  listener($('#click2')).domEventListener(
+    'click', document.getElementById('click2'));
+
+  listener($('#click3')).domEventHandler(
+    'click', document.getElementById('click3'));
+
+  listener($('#input1')).jQueryEvent(
+    'input', '#grocery');
+
+  listener($('#input2')).domEventListener(
+    'input', document.getElementById('grocery'));
+
+  listener($('#input3')).domEventHandler(
+    'input', document.getElementById('grocery'));
+
+  $('#grocery').on('keyup', function(event) {
     var grocery;
-    var mouseover1;
-    var mouseover2;
-    var mouseover3;
 
-    listener($('#click1')).jQueryEvent(
-      'click', '#click1');
+    if (event.which == 13) {
+      grocery = $(this).val();
+      $(this).val('');
 
-    listener($('#click2')).domEventListener(
-      'click', document.getElementById('click2'));
-
-    listener($('#click3')).domEventHandler(
-      'click', document.getElementById('click3'));
-
-    listener($('#input1')).jQueryEvent(
-      'input', '#grocery');
-
-    listener($('#input2')).domEventListener(
-      'input', document.getElementById('grocery'));
-
-    listener($('#input3')).domEventHandler(
-      'input', document.getElementById('grocery'));
-
-    $('#grocery').on('keyup', function(event) {
-      if (event.which == 13) {
-        grocery = $(this).val();
-        $(this).val('');
-
-        if (grocery) {
-          $('#list').append('<li>' + grocery + '</li>');
-          showNodes();
-        }
-      }
-    });
-
-    $('#list').click(function(event) {
-      var target = getTarget(event);
-
-      if (target.tagName === 'LI') {
-        target.parentNode.removeChild(target);
+      if (grocery) {
+        $('#list').append('<li class="grocery">' + grocery + '</li>');
         showNodes();
       }
-    });
+    }
+  });
 
-    mouseover1 = listener($('#mouseover1'));
-    mouseover2 = listener($('#mouseover2'));
-    mouseover3 = listener($('#mouseover3'));
+  $('#list').click(function(event) {
+    var target = getTarget(event);
+
+    if (target.tagName === 'LI') {
+      target.parentNode.removeChild(target);
+      showNodes();
+    }
+  });
+
+  // Event object
+  (function() {
+    var mouseover1 = listener($('#mouseover1'));
+    var mouseover2 = listener($('#mouseover2'));
+    var mouseover3 = listener($('#mouseover3'));
+    var showId;
 
     mouseover1.jQueryEvent(
       'mouseover', '#howTo');
@@ -90,17 +99,6 @@ $(document).ready(function() {
 
     mouseover3.domEventHandler(
       'mouseover', document.getElementById('howTo'));
-
-    getTarget = function(event) {
-      var target;
-      event = event || window.event;
-
-      if (event) {
-        target = event.target || event.srcElement;
-      }
-
-      return target;
-    };
 
     showId = function(element, event) {
       var target = getTarget(event);
@@ -122,5 +120,66 @@ $(document).ready(function() {
     mouseover3.callback = function(event) {
       showId(this.getElement(), event);
     };
+  }());
+
+  // Changing default behavior
+  (function() {
+    var click4 = listener($('#click4'));
+    var click5 = listener($('#click5'));
+    var click6 = listener($('#click6'));
+    var blockLink;
+    var followLink;
+    var ie;
+
+    click4.jQueryEvent(
+      'click', '#noLinks');
+
+    click5.domEventListener(
+      'click', document.getElementById('noLinks'));
+
+    click6.domEventHandler(
+      'click', document.getElementById('noLinks'));
+
+    blockLink = function(event) {
+      if (event.preventDefault) {
+        event.preventDefault();
+      } else {
+        event.returnValue = false;
+      }
+    };
+
+    click4.callback = function(event) {
+      blockLink(event);
+    };
+
+    click5.callback = function(event) {
+      blockLink(event);
+    };
+
+    click6.callback = function(event) {
+      blockLink(event);
+    };
+
+    followLink = function(event) {
+      if (event.stopPropagation) {
+        event.stopPropagation();
+      } else {
+        event.cancelBubble = true;
+      }
+    };
+
+    ie = document.getElementById('IE');
+
+    if (ie.addEventListener) {
+      ie.addEventListener('click', function(event) {
+        followLink(event);
+      }, false);
+    }
+
+    if (ie.attachEvent) {
+      ie.attachEvent('onclick', function(event) {
+        followLink(event);
+      });
+    }
   }());
 });
