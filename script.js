b6b18b47901c18294969b6897003503ed1de35dd
Hiroshi Nagata <hirosn@live.com>
2014-10-14 15:00:44 -0700

    Add notes on memoization.

--- script.js
+++ script.js
@@ -14,6 +14,58 @@ $(document).ready(function() {
     $(this).removeClass('active');
   });
 
+  // Memoization in Closure
+  (function() {
+    var fibonacci = function() {
+      var cache = [0, 1];
+
+      return function fib(n) {
+        var r;
+
+        if (n >= 0) {
+          r = cache[n];
+
+          if (typeof r !== 'number') {
+            r = fib(n - 1) + fib(n - 2);
+            cache[n] = r;
+          }
+        }
+
+        return r;
+      };
+    }();
+
+    var showAnswer = function(n) {
+      var answer;
+
+      if (typeof n === 'number') {
+        answer = fibonacci(n);
+      }
+
+      if (typeof answer !== 'number') {
+        answer = '?';
+      }
+
+      $('#fibans').text(answer);
+    };
+
+    $('#fibn').on('focus', function() {
+      $(this).val('');
+      showAnswer();
+    });
+
+    $('#fibn').on('blur', function() {
+      showAnswer(+$(this).val());
+    });
+
+    $('#fibn').on('keyup', function(event) {
+      if (event.which == 13) {
+        showAnswer(+$(this).val());
+        $(this).blur();
+      }
+    });
+  }());
+
   // Event listeners and handlers
   (function() {
     listener($('#click1')).jQueryEvent(
