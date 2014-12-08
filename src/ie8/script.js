e4270e9ffcb966c0bc47d4d35911f9c91a1f1d54
Hiroshi Nagata <hirosn@live.com>
2014-12-08 14:13:08 -0800

    Cleanup as this file is now small enough to be self explanatory.

--- src/ie8/script.js
+++ src/ie8/script.js
@@ -3,27 +3,13 @@
 /* global $: false */
 
 $(document).ready(function() {
-  var showNodes = require('./js/shownodes');
-
-  // Timestamp
-  $('#timestamp').text(document.lastModified);
-
-  // Enable elements
-  $('input').removeAttr('disabled');
-
-  // Whitespace nodes in children of ul element
-  showNodes();
-  $('#page').append('<p>' + navigator.userAgent + '</p');
-
-  // Event listeners and handlers
+  require('./js/shownodes')();
   require('./js/eventhandlers')();
-
-  // Event object
   require('./js/eventobject')();
-
-  // Changing default behavior
   require('./js/defaultbehavior')();
-
-  // Computed style
   require('./js/computedstyle')();
+
+  $('input').removeAttr('disabled');
+  $('#page').append('<p>' + navigator.userAgent + '</p');
+  $('#timestamp').text(document.lastModified);
 });
