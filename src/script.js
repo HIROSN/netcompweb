dd858a858bfd0271d91d7700e1cd79e16bfbdee7
Hiroshi Nagata <hirosn@live.com>
2017-05-01 00:40:02 -0700

    add toggle

--- src/script.js
+++ src/script.js
@@ -6,6 +6,10 @@ var app = angular.module('webApp', ['webApp.templates']);
 
 app.footerLoaded = function() {
   $('.delayed').fadeIn('fast');
+  var $h3s = $('article > section > h3');
+  $h3s.on('click', function() {
+    $h3s.toggleClass('sticky').addClass('noselect');
+  });
 };
 
 require('./js/directives/aside')(app);
