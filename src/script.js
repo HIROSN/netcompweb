107ce20d3d3d24b1f67ed758de17f4c4edaf9fd3
Hiroshi Nagata <hirosn@live.com>
2017-05-01 01:01:19 -0700

    toggle article style

--- src/script.js
+++ src/script.js
@@ -7,8 +7,8 @@ var app = angular.module('webApp', ['webApp.templates']);
 app.footerLoaded = function() {
   $('.delayed').fadeIn('fast');
   var $h3s = $('article > section > h3');
-  $h3s.on('click', function() {
-    $h3s.toggleClass('sticky').addClass('noselect');
+  $h3s.addClass('noselect').on('click', function() {
+    $('article').toggleClass('sticky');
   });
 };
 
